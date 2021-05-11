0ed18cd170b1f5f521ebaf4abbd755efede6bbe5
Lourdes Mendoza <lourd.mendoza@gmail.com>
2021-05-10 22:59:05 -0600

    Create methods and tests for Turn files

--- src/Turn.js
+++ src/Turn.js
@@ -1,5 +1,35 @@
+class Turn {
+  constructor(guess, card) {
+    this.guess = guess
+    this.card = card
+  }
+
+  returnGuess() {
+    return this.guess
+  }
+
+  returnCard() {
+    return this.card
+  }
+
+  evaluateGuess() {
+    if (this.guess !== this.card.correctAnswer) {
+      return false
+    }
+    return true
+  }
 
 
+  giveFeedback() {
+    if (this.evaluateGuess() === true) {
+      return "You got it!"
+    }
+    return "Sorry, that was wrong."
+  }
+
+
+
+}
 
 
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -3,3 +3,59 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+
+describe('Turn', function () {
+  let card;
+  let turn;
+
+  beforeEach(() => {
+    card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    turn = new Turn('pug', card);
+   });
+
+   it('should be a function', function() {
+     expect(Turn).to.be.a('function')
+   });
+
+  it('should be an instance of Turn', function() {
+    expect(turn).to.be.an.instanceof(Turn);
+  })
+
+  it('should store a store guess value and Card object', function() {
+
+    expect(turn.guess).to.equal('pug')
+    expect(turn.card).to.equal(card)
+  })
+
+  it('should return a guess', function() {
+    turn.returnGuess();
+    expect(turn.returnGuess()).to.equal('pug')
+  })
+
+  it('should return a card object', function() {
+    turn.returnCard();
+    expect(turn.returnCard()).to.equal(card)
+  })
+
+  it('should delclare if user guess maches correct answer or not', function() {
+    // const card = new Card('sea otter')
+
+    turn.evaluateGuess()
+    expect(turn.evaluateGuess()).to.equal(false);
+  })
+
+  it('should give affirmative or negative feedback depending on guess', function () {
+    turn.giveFeedback()
+
+    expect(turn.giveFeedback()).to.equal("Sorry, that was wrong.")
+
+
+  })
+
+
+
+
+
+
+});
